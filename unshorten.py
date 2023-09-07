063f772a5fdde04c4061ed2ad1ef915a305c729a
Devilsaint <anshul.suresh99@gmail.com>
2023-09-08 01:14:35 +0530

    added error support

--- unshorten.py
+++ unshorten.py
@@ -11,7 +11,7 @@ else:
         shortIt.hyper()
         st.write(f'Original URL: {shortIt.data}')
     except Exception as e:
-        st.error("Opps.. an error occurred.\nIt is either due to\n1. Our site couldn't handle link.\n2. Link might be broken.")
+        st.error(f"Opps.. an error occurred.{e}")
         st.sidebar.write("\n\nIf you feel our site could't handle the link please help to log an issue on [issue](https://github.com/devilsaint99/Unshortener/issues) in our github.")
 
 
